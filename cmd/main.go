d3a3efde13c9ff531e9d300c94abde5750e84521
Kollin <kollinchu@gmail.com>
2018-09-04 14:09:02 +0800

    update vendor & some code

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"flag"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	vsphereHost = flag.String("vsphere_host", "", "")
+	metricPath  = flag.String("metric_path", "/metrics", "Metrics Path")
+	VERSION = "<<< filled in by build >>>"
+)
+
 func main(){
+
+	flag.Parse()
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+<html>
+<head><title>VMWare Exporter v` + VERSION + `</title></head>
+<body>
+<h1>Redis Exporter ` + VERSION + `</h1>
+<p><a href='` + *metricPath + `'>Metrics</a></p>
+</body>
+</html>
+						`))
+	})
 	
+	http.ListenAndServe(":9272", nil)
 }
---
+++ pkg/exporter.go
@@ -0,0 +1,65 @@
+package pkg
+
+import (
+	"sync"
+	"context"
+
+	"github.com/vmware/govmomi"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+
+type Exporter struct {
+	vsphereHost string
+	vsphereUser string
+	vspherePassword string
+	ignoreSSL bool
+	metrics  map[string]*prometheus.GaugeVec
+	sync.RWMutex
+	namespace string
+}
+// NewVMwareExporter returns a new exporter of vmware metrics.
+func NewVMwareExporter(vsphereHost, vsphereUser, vspherePassword string, ignoreSSL bool)(*Exporter, error){
+
+	e := Exporter{
+		vsphereHost: vsphereHost,
+		vsphereUser: vsphereUser, 
+		vspherePassword: vspherePassword,
+		ignoreSSL: ignoreSSL,
+	}
+	e.initGauges()
+	return &e, nil
+}
+
+func (e *Exporter) initGauges(){
+	e.metrics = map[string]*prometheus.GaugeVec{}
+	// e.metrics[""] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		
+	// })
+}
+
+// Describe outputs VMware metric descriptions.
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+
+	for _, m := range e.metrics {
+		m.Describe(ch)
+	}
+
+	// ch <- e.duration.Desc()
+	// ch <- e.totalScrapes.Desc()
+	// ch <- e.scrapeErrors.Desc()
+}
+
+// Collect fetches new metrics from the RedisHost and updates the appropriate metrics.
+func (e *Exporter)Collect(ch chan<- prometheus.Metric){
+
+}
+
+func (e *Exporter) scrapeVMware(){
+
+}
+
+func(e *Exporter) newVMwareClient(ctx context.Context)(*govmomi.Client, error){
+	
+	return &govmomi.NewClient(), nil
+}
